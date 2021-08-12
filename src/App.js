fd4f1a5ca33eeaac997093635f062d18ca752e5b
Mike Purvis <mikedotexe@gmail.com>
2021-08-12 15:16:31 -0700

    initial commit of decent working MVP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+import React, { useCallback, useRef, useState } from 'react';
+import Crossword from '@jaredreisinger/react-crossword';
 
 function App() {
+  const crossword = useRef();
+
+  const [messages, setMessages] = useState([]);
+
+  const addMessage = useCallback((message) => {
+    setMessages((m) => m.concat(`${message}\n`));
+  }, []);
+
+  const onCrosswordComplete = useCallback(
+    (isComplete) => {
+      addMessage(`onCrosswordComplete: ${JSON.stringify(isComplete)}`);
+    },
+    [addMessage]
+  );
+
+  const onCellChange = useCallback(
+    (row, col, char) => {
+      addMessage(`onCellChange: "${row}", "${col}", "${char}"`);
+    },
+    [addMessage]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page">
+      <h1>@jaredreisinger/react-crossword example app</h1>
+
+      <p>
+        This is a demo app that makes use of the @jaredreisinger/react-crossword
+        component. It exercises most of the functionality, so that you can see
+        how to do so.
+      </p>
+
+      <div id="crossword-wrapper">
+        <Crossword
+          data={data}
+          ref={crossword}
+          onCrosswordComplete={onCrosswordComplete}
+          onCellChange={onCellChange}
+        />
+      </div>
+
+      <pre id="messages">{messages}</pre>
     </div>
   );
 }
